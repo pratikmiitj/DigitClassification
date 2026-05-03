b0e3615403363b260183454497e5d488f1fea7df
pratikmiitj <114722829+pratikmiitj@users.noreply.github.com>
2026-05-03 12:29:06 +0530

    Update plot_digits_classification.py change test split to 0.2

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -64,7 +64,7 @@ clf = svm.SVC(gamma=0.001)
 
 # Split data into 50% train and 50% test subsets
 X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
+    data, digits.target, test_size=0.2, shuffle=False
 )
 
 # Learn the digits on the train subset
